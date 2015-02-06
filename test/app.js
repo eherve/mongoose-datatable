710a2e36be9485af22864e09ceae1caa60c9f3d6
Eric Herve <eric.herve.95@gmail.com>
2015-02-06 21:43:31 +0100

    update tests

--- test/app.js
+++ test/app.js
@@ -22,10 +22,21 @@ db.once('open', function() {
 
   // Load Model
   var Schema = mongoose.Schema;
+  var subSchema = new Schema({
+    id: { type: String, unique: true },
+    description: String
+  });
+  var subModel = mongoose.model('SubTest', subSchema);
+  var subElement = new Schema({
+    type: { type: Schema.Types.ObjectId, ref: 'SubTest' }
+  });
   var schema = new Schema({
     str: String, date: Date, bool: Boolean, num: Number,
     select_false: { type: String, select: false },
-    datatable_select_false: { type: String, dataTableSelect: false }
+    datatable_select_false: { type: String, dataTableSelect: false },
+    type: { type: Schema.Types.ObjectId, ref: 'SubTest' },
+    types: [ subElement ],
+    elements: [ { type: { type: Schema.Types.ObjectId, ref: 'SubTest' } } ] // Does not work
   });
   var model = mongoose.model('Test', schema);
 
--- test/public/js/index.js
+++ test/public/js/index.js
@@ -5,16 +5,41 @@ $(document).ready(function() {
     bServerSide: true,
     sAjaxSource: "/data",
     aoColumns: [
-      { mData: "str" },
-      { mData: "date" },
+      { mData: "str", sDefaultContent: "" },
+      { mData: "date", sDefaultContent: "" },
       { sName: 'bool', sDefaultContent: "",
         mRender: function (data, type, full) {
           return full.bool;
         }
       },
-      { mData: "num" },
+      { mData: "num", sDefaultContent: "" },
       { mData: "select_false", sDefaultContent: "not fetched" },
-      { mData: "datatable_select_false", sDefaultContent: "not fetched" }
+      { mData: "datatable_select_false", sDefaultContent: "not fetched" },
+      { mData: "type", sDefaultContent: "",
+        mRender: function(data, type, full) {
+          return data && data._id ? (data.id + " - " + data.description) : "";
+        }
+      },
+      { mData: "types.type", sDefaultContent: "",
+        mRender: function(data, type, full) {
+          if (!full.types) { return ""; }
+          var str = "";
+          for (var i = 0; i < full.types.length; ++i) {
+            str += full.types[i].type.id + " - " + full.types[i].type.description + " ";
+          }
+          return str;
+        }
+      },
+      { mData: "elements.type", sDefaultContent: "", // Does not work
+        mRender: function(data, type, full) {
+          if (!full.elements) { return ""; }
+          var str = "";
+          for (var i = 0; i < full.elements.length; ++i) {
+            str += full.elements[i].type.id + " - " + full.elements[i].type.description + " ";
+          }
+          return str;
+        }
+      }
     ],
     fnServerParams: function(aoData) {
       aoData.push({ name: "bChunkSearch", value: true }); }
