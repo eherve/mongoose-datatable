9b8b0d506e3a7c7f41b1c7bc307c27aa5c02b728
Eric Her <eric.herve.95@gmail.com>
2019-03-05 09:35:00 +0100

    add declaration file

---
+++ dist/datatable.d.ts
@@ -0,0 +1,86 @@
+import { Schema } from 'mongoose';
+interface ILogger {
+    debug: (...data: any) => void;
+    info: (...data: any) => void;
+    warn: (...data: any) => void;
+    error: (...data: any) => void;
+}
+export interface IColumn {
+    data: string;
+    name: string;
+    searchable: boolean;
+    orderable: boolean;
+    search: ISearch;
+}
+interface IOrder {
+    column: number;
+    dir: string;
+}
+export interface ISearch {
+    value: string;
+    regex: boolean;
+    chunks?: string[];
+}
+export interface IQuery {
+    draw: string;
+    columns: IColumn[];
+    order?: IOrder[];
+    start: string;
+    length: string;
+    search: ISearch;
+}
+export declare type HandlerType = (query: IQuery, column: IColumn, field: any, search: ISearch, global: boolean) => any;
+export interface IOptions {
+    logger?: ILogger;
+    handlers?: {
+        [type: string]: HandlerType;
+    };
+    conditions?: any;
+    select?: any;
+}
+export interface IData {
+    draw: string;
+    recordsTotal: number;
+    recordsFiltered: number;
+    data: any[];
+}
+export interface IConfig {
+    logger?: ILogger;
+    handlers?: {
+        [type: string]: HandlerType;
+    };
+}
+declare class DataTableModule {
+    private schema;
+    static CONFIG: IConfig;
+    private _config;
+    private readonly config;
+    private readonly logger;
+    private model;
+    static configure(config?: IConfig): IConfig;
+    static init(schema: any, opts: any): void;
+    constructor(schema: Schema);
+    private dataTable;
+    private buildSort;
+    private updateAggregateOptions;
+    private fetchField;
+    private getField;
+    private addProjection;
+    private addSearch;
+    private getSearch;
+    private chunkSearch;
+    private buildColumnSearch;
+    private buildColumnSearchString;
+    private buildColumnSearchBoolean;
+    private buildColumnSearchNumber;
+    private buildColumnSearchDate;
+    private buildColumnSearchObjectId;
+    private pagination;
+    private isTrue;
+    private isFalse;
+    private isSelectable;
+    private recordsTotal;
+    private recordsFiltered;
+    private data;
+}
+export default DataTableModule;
